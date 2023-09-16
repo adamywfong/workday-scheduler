aeeba7117aa410be67391507259ad3d6ff60b0c1
afong <adam.y.w.fong@gmail.com>
2023-09-16 11:27:33 -0700

    Added current day display to header

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+//Displays current day in Day, Month # format
+$('#currentDay').text(dayjs().format('dddd, MMMM D'));
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
